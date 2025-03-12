05554c972762cd927750cf83b11e5c3aecb91311
M. Fahrur Rizal <fahrurrzl5798@gmail.com>
2025-03-12 09:19:47 +0700

    feat: Send email

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -29,7 +29,13 @@ const registerValidateSchema = Yup.object({
 });
 
 export default {
+  /**
+    #swagger.tags = ['Auth']
+   */
   async register(req: Request, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     */
     const { fullName, username, email, password, confirmPassword } =
       req.body as unknown as TRegister;
 
@@ -63,6 +69,7 @@ export default {
 
   async login(req: Request, res: Response) {
     /**
+     #swagger.tags = ['Auth']
      #swagger.requestBody = {
         required: true,
         schema: {$ref: "#/components/schemas/LoginRequest"}
@@ -120,6 +127,7 @@ export default {
 
   async me(req: IReqUser, res: Response) {
     /**
+      #swagger.tags = ['Auth']
       #swagger.security = [{
         "bearerAuth": []
       }]
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { encrypt } from "../utils/encryption";
+import { renderMailHtml, sendEmail } from "../utils/mail/mail";
+import { CLIENT_HOST, EMAIL_SMTP_USER } from "../utils/env";
 
 export interface User {
   fullName: string;
@@ -10,6 +12,7 @@ export interface User {
   profilePicture: string;
   isActive: boolean;
   activationCode: string;
+  createdAt?: string;
 }
 
 const Schema = mongoose.Schema;
@@ -58,6 +61,33 @@ UserSchema.pre("save", function (next) {
   const user = this;
   user.password = encrypt(user.password);
   next();
+});
+
+UserSchema.post("save", async function (doc, next) {
+  try {
+    const user = doc;
+
+    console.log("Send email to", user.email);
+
+    const contentMail = await renderMailHtml("registration-success.ejs", {
+      username: user.username,
+      fullName: user.fullName,
+      email: user.email,
+      createdAt: user.createdAt,
+      activationLink: `${CLIENT_HOST}/auth/activation?code=${user.activationCode}`,
+    });
+
+    await sendEmail({
+      from: EMAIL_SMTP_USER,
+      to: user.email,
+      subject: "Aktivasi aku Anda",
+      html: contentMail,
+    });
+  } catch (error) {
+    console.log("error", error);
+  } finally {
+    next();
+  }
 });
 
 UserSchema.methods.toJSON = function () {
