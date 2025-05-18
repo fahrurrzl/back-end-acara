d099cfb1ed431d0c082e02452e19ae72b5ea17b3
fahrurrzl <fahrurrzl5798@gmail.com>
2025-05-18 09:39:07 +0700

    feat: api order

---
+++ src/models/order.model.ts
@@ -0,0 +1,119 @@
+import mongoose, { ObjectId, Schema } from "mongoose";
+import * as Yup from "yup";
+
+import { EVENT_MODEL_NAME } from "./event.model";
+import { USER_MODEL_NAME } from "./user.model";
+import { TICKET_MODEL_NAME } from "./ticket.model";
+import payment, { TypeResponseMidtrans } from "../utils/payment";
+import { getId } from "../utils/id";
+
+export const ORDER_MODEL_NAME = "Order";
+
+export const orderDAO = Yup.object({
+  createdBy: Yup.string().required(),
+  events: Yup.string().required(),
+  ticket: Yup.string().required(),
+  quantity: Yup.number().required(),
+});
+
+export type TOrder = Yup.InferType<typeof orderDAO>;
+
+export enum OrderStatus {
+  PENDING = "pending",
+  COMPLETED = "completed",
+  CANCELLED = "cancelled",
+}
+
+export type TypeVoucher = {
+  voucherId: string;
+  isPrint: boolean;
+};
+
+export interface IOrder
+  extends Omit<TOrder, "createdBy" | "ticket" | "events"> {
+  total: number;
+  status: string;
+  payment: TypeResponseMidtrans;
+  createdBy: ObjectId;
+  events: ObjectId;
+  orderId: string;
+  ticket: ObjectId;
+  quantity: number;
+  vouchers: TypeVoucher[];
+}
+
+const OrderSchema = new Schema<IOrder>(
+  {
+    orderId: {
+      type: Schema.Types.String,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: USER_MODEL_NAME,
+      required: true,
+    },
+    events: {
+      type: Schema.Types.ObjectId,
+      ref: EVENT_MODEL_NAME,
+      required: true,
+    },
+    total: {
+      type: Schema.Types.Number,
+      required: true,
+    },
+    payment: {
+      type: {
+        token: {
+          type: Schema.Types.String,
+          required: true,
+        },
+        redirect_url: {
+          type: Schema.Types.String,
+          required: true,
+        },
+      },
+    },
+    status: {
+      type: Schema.Types.String,
+      enum: [OrderStatus.PENDING, OrderStatus.COMPLETED, OrderStatus.CANCELLED],
+      default: OrderStatus.PENDING,
+    },
+    ticket: {
+      type: Schema.Types.ObjectId,
+      ref: TICKET_MODEL_NAME,
+      required: true,
+    },
+    quantity: {
+      type: Schema.Types.Number,
+      required: true,
+    },
+    vouchers: {
+      type: [
+        {
+          voucherId: {
+            type: Schema.Types.String,
+          },
+          isPrint: {
+            type: Schema.Types.Boolean,
+            default: false,
+          },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+).index({ orderId: "text" });
+
+OrderSchema.pre("save", async function () {
+  const order = this;
+  order.orderId = getId();
+  order.payment = await payment.createLink({
+    transaction_detail: {
+      gross_amount: order.total,
+      order_id: order.orderId,
+    },
+  });
+});
+
+const OrderModel = mongoose.model(ORDER_MODEL_NAME, OrderSchema);
+export default OrderModel;
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const DATABASE_URL: string = process.env.DATABASE_URL || "";
 export const SECRET: string = process.env.SECRET || "";
+
 export const EMAIL_SMTP_SECURE: boolean =
   Boolean(process.env.EMAIL_SMTP_SECURE) || false;
 export const EMAIL_SMTP_PASS: string = process.env.EMAIL_SMTP_PASS || "";
@@ -21,3 +22,8 @@ export const CLOUDINARY_CLOUD_NAME: string =
 export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
 export const CLOUDINARY_API_SECRET: string =
   process.env.CLOUDINARY_API_SECRET || "";
+
+export const MIDTRANS_CLIENT_KEY = process.env.MIDTRANS_CLIENT_KEY || "";
+export const MIDTRANS_SERVER_KEY = process.env.MIDTRANS_SERVER_KEY || "";
+export const MIDTRANS_TRANSACTION_URL =
+  process.env.MIDTRANS_TRANSACTION_URL || "";
