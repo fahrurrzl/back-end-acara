65de627a427950c31ed17f71aa6348c3fb04a058
fahrurrzl <fahrurrzl5798@gmail.com>
2025-05-25 09:02:23 +0700

    fix: cors payment

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,13 @@ async function init() {
 
     const app = express();
 
-    app.use(cors());
+    app.use(
+      cors({
+        origin: ["http://localhost:3000", "https://acara-black.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+      })
+    );
     app.use(bodyParser.json());
 
     const PORT = 3001;
